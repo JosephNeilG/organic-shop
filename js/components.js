5f81846604e912dbd79fdf104bc90fd30b698216
Joseph Neil Gapuz <josephneil873@gmail.com>
2025-03-11 20:06:17 +0800

    add initial progress signup, login modal

--- js/components.js
+++ js/components.js
@@ -1,13 +1,22 @@
-function loadComponent(id, file) {
+function loadComponent(id, file, callback) {
     fetch(file)
         .then(response => response.text())
-        .then(data => document.getElementById(id).innerHTML = data)
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+            if (callback) callback();
+        })
         .catch(error => console.error(`Error loading ${file}:`, error));
 }
 
-// Load components
 document.addEventListener("DOMContentLoaded", function () {
     loadComponent("sidebar", "components/sidebar.html");
-    loadComponent("navbar", "components/navbar.html");
+    loadComponent("navbar", "components/navbar.html", function () {
+        fetch("components/modals.html")
+            .then(response => response.text())
+            .then(modalData => {
+                document.body.insertAdjacentHTML("beforeend", modalData);
+            })
+            .catch(error => console.error("Error loading modals:", error));
+    });
     loadComponent("search-cart", "components/search-cart.html");
 });
