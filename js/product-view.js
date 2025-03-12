51fd733883de05305b05b4ff957ae07ba288a6b6
oronorskie <aaranarawork@gmail.com>
2025-03-12 14:54:16 +0800

    changed price and description

--- js/product-view.js
+++ js/product-view.js
@@ -24,7 +24,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     quantityInput.addEventListener("input", function () {
         let quantity = parseInt(quantityInput.value) || 1;
-        let price = parseFloat(product.price.replace("$", ""));
-        totalAmount.value = "$" + (quantity * price).toFixed(2);
+        let price = parseFloat(product.price.replace("₱", ""));
+        totalAmount.value = "₱" + (quantity * price).toFixed(2);
     });
 });
